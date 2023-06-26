3636becf4a5ae83d31bd4072584117d0b60ce736
Dai <87512311+DaiTNgo@users.noreply.github.com>
2023-06-26 21:46:50 +0700

    react-buoi-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card from "./modules/cach-tao-component/card";
+import RccComponent from "./modules/cach-tao-component/rcc-component";
+import RfcComponent from "./modules/cach-tao-component/rfc-component";
 
+import HomeLayout from "./modules/home-layout/home-layout";
+import BindingData from "./modules/binding-data/binding-data";
+// đây là file, tạo component App để đưa lên giao diện.
+// js + css + html => .jsx
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BindingData />
+      {/* <HomeLayout /> */}
+      {/* <h1>Hello World</h1> */}
+      {/* 1. */}
+      {/* <RccComponent /> */}
+      {/* <input /> */}
+
+      {/* 2. */}
+      {/* <RccComponent></RccComponent> */}
+      {/* <p></p> */}
+
+      {/* <RfcComponent /> */}
+
+      {/* <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
